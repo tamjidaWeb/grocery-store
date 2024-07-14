5d12954a43894ba8dfcaba3653f9f2964b42c378
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-07-14 18:15:05 +0600

    commited

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ document.querySelector('#search-btn').onclick =() =>
 {
     searchForm.classList.toggle('active');
 }
+let shoppingCart =document.querySelector('.shopping-cart');
+document.querySelector('#cart-btn').onclick =() =>
+{
+    shoppingCart.classList.toggle('active');
+}
+let logIn =document.querySelector('.login-form');
+document.querySelector('#login-btn').onclick =() =>
+{
+    logIn.classList.toggle('active');
+}
