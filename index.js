f5d368b63bbdd65890e33e0ab79dbf738a045ed1
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-07-16 11:31:48 +0600

    commited

--- index.js
+++ index.js
@@ -2,14 +2,42 @@ let searchForm =document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick =() =>
 {
     searchForm.classList.toggle('active');
+    
+    shoppingCart.classList.remove('active');
+    logIn.classList.remove('active');
+    navBar.classList.remove('active');
 }
 let shoppingCart =document.querySelector('.shopping-cart');
 document.querySelector('#cart-btn').onclick =() =>
 {
     shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    logIn.classList.remove('active');
+    navBar.classList.remove('active');
 }
 let logIn =document.querySelector('.login-form');
 document.querySelector('#login-btn').onclick =() =>
 {
     logIn.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navBar.classList.remove('active');
 }
+
+let navBar =document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick =() =>
+{
+    navBar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    logIn.classList.remove('active');
+
+}
+
+window.onscroll =() =>
+    {
+        searchForm.classList.remove('active');
+        shoppingCart.classList.remove('active');
+        logIn.classList.remove('active');
+        navBar.classList.remove('active');
+    }
