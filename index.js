2687e76be320df076715647643f38f749eaec03a
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-07-17 13:37:40 +0600

    blog part completed

--- index.js
+++ index.js
@@ -66,3 +66,31 @@ window.onscroll =() =>
           },
         },
       });
+
+
+
+
+
+      var swiper = new Swiper(".review-slider", {
+        loop:true,
+        spaceBetween: 20,
+        autoplay:{
+            delay: 5500,
+            disableOnInteraction: false,
+        },
+    
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+            
+          },
+          768: {
+            slidesPerView: 2,
+           
+          },
+          1020: {
+            slidesPerView: 3,
+           
+          },
+        },
+      });
